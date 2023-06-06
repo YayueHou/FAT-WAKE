4e75e3954b9c50f46d9689d35d16cbeb286ad134
YayueHou <61933621+YayueHou@users.noreply.github.com>
2023-06-06 14:45:24 +0800

    add data file lists

---
+++ data_process/EEGFileList.py
@@ -0,0 +1,145 @@
+FATIGUE_US = [
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_04_FAT.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_06_FAT.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_08_FAT.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_09_FAT.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_11_FAT.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_12_FAT.txt"
+]
+FATIGUE_BL = [
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_01_FAT.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_02_FAT.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_03_FAT.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_04_FAT.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_05_FAT.txt"
+]
+FATIGUE_UL = [
+   "FAT_WAK_data/FW_UMINDLITE/UmindLite_01_FAT.csv",
+   "FAT_WAK_data/FW_UMINDLITE/UmindLite_02_FAT.csv",
+   "FAT_WAK_data/FW_UMINDLITE/UmindLite_06_FAT.csv",
+   "FAT_WAK_data/FW_UMINDLITE/UmindLite_09_FAT.csv",
+   "FAT_WAK_data/FW_UMINDLITE/UmindLite_10_FAT.csv",
+]
+# EMOTIV 128Hz
+FATIGUE_EM = [
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_01_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_02_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_03_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_04_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_05_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_06_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_07_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_08_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_09_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_10_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_11_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_12_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_13_FAT.csv",
+ #  "FAT_WAK_data/FW_EMOTIV/Emotiv_14_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_15_FAT.csv",
+   "FAT_WAK_data/FW_EMOTIV/Emotiv_16_FAT.csv",
+]
+WAKE_US = [
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_04_WAK.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_06_WAK.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_08_WAK.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_09_WAK.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_11_WAK.txt",
+    "FAT_WAK_data/FW_UMINDSLEEP/UmindSleep_12_WAK.txt"
+]
+WAKE_BL = [
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_01_WAK.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_02_WAK.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_03_WAK.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_04_WAK.txt",
+   "FAT_WAK_data/FW_BRAINLINK/BrainLinkRaw_05_WAK.txt"
+]
+WAKE_EM=[
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_01_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_02_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_03_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_04_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_05_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_06_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_07_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_08_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_09_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_10_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_11_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_12_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_13_WAK.csv",
+  #"FAT_WAK_data/FW_EMOTIV/Emotiv_14_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_15_WAK.csv",
+  "FAT_WAK_data/FW_EMOTIV/Emotiv_16_WAK.csv",
+]
+WAKE_UL=[
+ # "FAT_WAK_data/FW_UMINDLITE/UmindLite_01_WAK.csv",
+  "FAT_WAK_data/FW_UMINDLITE/UmindLite_02_WAK.csv",
+  "FAT_WAK_data/FW_UMINDLITE/UmindLite_06_WAK.csv",
+  "FAT_WAK_data/FW_UMINDLITE/UmindLite_09_WAK.csv",
+  "FAT_WAK_data/FW_UMINDLITE/UmindLite_10_WAK.csv",
+]
+
+FRONT_PATH = [
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore1.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore2.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore3.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore4.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore5.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore6.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore7.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore8.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore9.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore10.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore11.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore12.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore13.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore14.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore15.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore16.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore17.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore18.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore19.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore20.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore21.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore22.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore23.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore24.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore25.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore26.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore27.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore28.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore29.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore30.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore31.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore32.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore33.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore34.txt",
+   "FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore35.txt","FAT_WAK_data/MUSIC_TGAM_FRONTHEAR/fore36.txt"
+]
+
+EAR_PATH = [
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post1.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post2.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post3.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post5.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post6.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post7.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post8.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post9.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post11.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post12.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post13.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post14.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post15.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post16.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post17.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post18.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post19.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post20.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post21.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post22.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post23.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post24.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post25.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post26.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post27.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post28.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post29.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post30.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post31.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post32.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post33.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post34.txt",
+   "FAT_WAK_data/MUSIC_TGAM_EAR/post35.txt","FAT_WAK_data/MUSIC_TGAM_EAR/post36.txt"
+]
+
+TGAM_F_WAK=[
+   "FAT_WAK_data\FW_TGAM\FRO_FAT06_WAK.txt",
+   "FAT_WAK_data\FW_TGAM\FRO_FAT21_WAK.txt",
+   "FAT_WAK_data\FW_TGAM\FRO_FAT20_WAK.txt",
+]
+TGAM_F_FAT=[
+   "FAT_WAK_data\FW_TGAM\FRO_FAT06_FAT.txt",
+   "FAT_WAK_data\FW_TGAM\FRO_FAT07_FAT.txt",
+   "FAT_WAK_data\FW_TGAM\FRO_FAT20_FAT.txt",
+   "FAT_WAK_data\FW_TGAM\FRO_FAT21_FAT.txt",
+]
+TGAM_E_WAK=[
+   "FAT_WAK_data\FW_TGAM\EAR_FAT06_WAK.txt",
+   "FAT_WAK_data\FW_TGAM\EAR_FAT20_WAK.txt",
+   "FAT_WAK_data\FW_TGAM\EAR_FAT21_WAK.txt",
+]
+TGAM_E_FAT=[
+   "FAT_WAK_data\FW_TGAM\EAR_FAT06_FAT.txt",
+   "FAT_WAK_data\FW_TGAM\EAR_FAT07_FAT.txt",
+   "FAT_WAK_data\FW_TGAM\EAR_FAT20_FAT.txt",
+   "FAT_WAK_data\FW_TGAM\EAR_FAT21_FAT.txt",
+]
